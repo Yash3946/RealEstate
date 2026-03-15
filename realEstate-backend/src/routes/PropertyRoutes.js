972efb4e97b40e78e92b8e88921afc469d39266f
Yash3946 <patelyash8262@gmail.com>
2026-03-15 17:59:07 +0530

    Remove node_modules

--- realEstate-backend/src/routes/PropertyRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router()
-const propertyController = require("../controllers/PropertyController")
-
-router.get("/properties",propertyController.getProperties)
-router.post("/save",propertyController.addProperty)
-router.put("/update/:id",propertyController.updateProperty)
-router.delete("/delete/:id",propertyController.deleteProperty)
-module.exports = router
