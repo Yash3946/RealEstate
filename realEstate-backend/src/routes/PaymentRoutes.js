52967dadad7567db0aefc87c4f162a3ea041fd24
Yash3946 <patelyash8262@gmail.com>
2026-03-15 17:39:28 +0530

    Delete realEstate-backend directory/.env

--- realEstate-backend/src/routes/PaymentRoutes.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router()
-const paymentSchema = require("../controllers/PaymentController")
-
-router.get("/payments",paymentSchema.getPayment);
-router.post("/addpayment",paymentSchema.addPayment);
-router.put("/update/:id",paymentSchema.updatepayment);
-router.delete("/delete/:id",paymentSchema.deletePayment);
-
-module.exports = router
